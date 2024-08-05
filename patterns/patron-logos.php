cd86d3670b3aca1fbab45828721983d08fb2d724
glasshalfpool <glasshalfpool@gmail.com>
2024-08-05 12:43:11 +0100

    Patron logos
    Flex container for patron logos

---
+++ patterns/patron-logos.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Title: Responsive grid of patron logos
+ * Slug: cirenhistfest/patron-logos
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"align":"wide","className":"flexible-logos-container","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide flexible-logos-container"><!-- wp:image {"id":86,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/the-arts-society-corinium.webp" alt="" class="wp-image-86"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":86,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/barn-theatre.webp" alt="" class="wp-image-86"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":85,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/calmsden-farms.webp" alt="" class="wp-image-85"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":85,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/cotswold-lake-brewing-company.webp" alt="" class="wp-image-85"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":84,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/primrose-prust.webp" alt="" class="wp-image-84"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":84,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/08/yiangou.webp" alt="" class="wp-image-84"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":83,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/07/patron-daniel-bingham-foundation.webp" alt="" class="wp-image-83"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":83,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-kings-head.webp" alt="" class="wp-image-83"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":82,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-ciren-civic-society.webp" alt="" class="wp-image-82"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":82,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-CAHS.webp" alt="" class="wp-image-82"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":81,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-rasoi.webp" alt="" class="wp-image-81"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":81,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-CCDT.webp" alt="" class="wp-image-81"/></figure>
+<!-- /wp:image --></div>
+
+<!-- wp:image {"id":81,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://cirencesterhistoryfestival.org/wp-content/uploads/2024/06/patron-HonCoGlos.webp" alt="" class="wp-image-81"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
